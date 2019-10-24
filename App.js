1debd1761ddbf6f9af8b7d008bbd741af486ddfe
ChrissRaz <razanamihoatrajeanchristian@gmail.com>
2019-10-24 23:55:18 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,32 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+
+const App: () => React$Node = () => {
+  return (
+   <Text> 
+     It's work!! just the first test for my todo app...
+   </Text>
+  );
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
